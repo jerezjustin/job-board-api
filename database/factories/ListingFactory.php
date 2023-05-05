c9bc0bda286f4d7572389621785b33b2bfcbc09e
Justin Jerez <justinjerez@hotmail.com>
2023-05-05 08:31:14 -0400

    Create factories and seed database

---
+++ database/factories/ListingFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Listing>
+ */
+class ListingFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $datetime = $this->faker->dateTimeBetween('-1 month');
+
+        $content = '';
+        for ($i = 0; $i < 5; $i++) {
+            $content .= '<p class="mb-4">' . $this->faker->paragraph . '</p>';
+        }
+
+        return [
+            'title' => $this->faker->jobTitle,
+            'company' => $this->faker->company,
+            'location' => $this->faker->country,
+            'logo' => '',
+            'is_highlighted' => $this->faker->boolean(chanceOfGettingTrue: 25),
+            'is_active' => true,
+            'content' => $content,
+            'apply_link' => $this->faker->url,
+            'created_at' => $datetime,
+            'updated_at' => $datetime,
+            'user_id' => User::factory()->create(),
+        ];
+    }
+}
