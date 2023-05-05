1d6fad35b4c0d52a3d7b89ff4d90c00330892f5e
Justin Jerez <justinjerez@hotmail.com>
2023-05-05 08:30:45 -0400

    Install spatie/laravel-sluggable package

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,12 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Tag extends Model
 {
     use HasFactory;
+    use HasSlug;
 
     protected $guarded = [];
+
+    public $timestamps = false;
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
     public function listings(): BelongsToMany
     {
